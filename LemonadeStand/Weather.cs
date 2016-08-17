748ce6c2af5e319017018aa0ee29e393fb2aee94
DamonYoung42 <damonyoung@wi.rr.com>
2016-08-17 13:52:33 -0500

    named temperature min/max values.

--- LemonadeStand/Weather.cs
+++ LemonadeStand/Weather.cs
@@ -11,6 +11,8 @@ namespace LemonadeStand
         public int temperature;
         public string conditions;
         public List<string> clouds = new List<string> { "Sunny", "Overcast", "Rainy" };
+        public int temperatureMin = 45;
+        public int temperatureMax = 100;
 
         public Weather()
         {
@@ -21,11 +23,11 @@ namespace LemonadeStand
         {
             Random random = new Random(DateTime.Now.Millisecond);
 
-            temperature = random.Next(50,110);
+            temperature = random.Next(temperatureMin,temperatureMax);
 
             Random index = new Random(DateTime.Now.Millisecond);
 
-            conditions = clouds[index.Next(0, 3)];
+            conditions = clouds[index.Next(0, clouds.Count())];
         }
 
     }
